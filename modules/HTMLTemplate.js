18329f468b8a0d3b639e3b2f3231cbbba54352a5
YoonJieut <qmdldos@gmail.com>
2023-10-11 00:20:58 +0900

    HTMLTemplate 작성 및 테스트 완료

--- modules/HTMLTemplate.js
+++ modules/HTMLTemplate.js
@@ -7,3 +7,27 @@ const makeLiTagPokemonList = require('./makeLi-model');
  */
 // console.log(_pokemonKorName);
 // console.log(typeof(makeLiTagPokemonList));
+
+const HTMLTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Pokemon List</h1>
+  <ul>
+    ${makeLiTagPokemonList(_pokemonKorName)}
+  </ul>
+</body>
+</html>
+`;
+
+// 정상적으로 조립됐나 확인
+// html을 제작하는 것이긴 하나, html로 해석되기 전에는 단순 문자열 조합일 뿐이다.
+// server가 html로 해설할 것
+// console.log(HTMLTemplate);
+
+module.exports = HTMLTemplate;
